60fc76b88f867bda66057ea0aed014ab8ccafb08
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-11 07:42:09 +0200

    Fix typo

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -32,7 +32,7 @@ async function insertCategoryData() {
     await Category.insertMany([
       { name: "Thai", image: "thai-food.jpg" },
       { name: "American", image: "american-food.jpg" },
-      { name: "Indian", image: "indian-food-jpg" },
+      { name: "Indian", image: "indian-food.jpg" },
       { name: "Chinese", image: "chinese-food.jpg" },
       { name: "Spanish", image: "spanish-food.jpg" },
       { name: "Mexican", image: "mexican-food.jpg" },
