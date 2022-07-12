61ec543eee76a5b894a6a77a41bf6d128c4ec605
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-12 16:08:10 +0200

    Add recipe route controller

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -14,7 +14,8 @@ exports.homepage = async (req, res) => {
     //Step 1: get the categories from the server.
     const limitNumber = 5;
     const categories = await Category.find().limit(limitNumber);
-    const recipes = await Recipe.find().limit(limitNumber);
+    const recipes = await Recipe.find().sort({ _id: -1 }).limit(limitNumber);
+    // Get the most recent 5 recipes (sort will sort them by descending order according to their auto-generated id number; bigger id number = more recent)
     res.render("index", { title: "Cooking Blog: Home", categories, recipes });
     //The above indicates that the homepage is created by rendering the data in the "index" file in the main.ejs file.  I've also specified a page title.   If you look at ./views/layouts/main.js you'll see where it wil be inserted)
   } catch (error) {
@@ -33,6 +34,18 @@ exports.exploreCategories = async (req, res) => {
     const categories = await Category.find();
     res.render("categories", { title: "Cooking Blog: Categories", categories });
     // This tells the server to return a rendered version of the "categories.ejs" page, which will be slotted into the "main.ejs" template.  The fact that main.ejs is being used was defined over on app.js
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error occurred" });
+  }
+};
+
+/* 
+GET recipe by ID
+*/
+exports.exploreRecipe = async (req, res) => {
+  try {
+    const recipe = await Recipe.find({ _id: req.params.id });
+    res.render("recipe", { title: "Cooking Blog: Recipe", recipe });
   } catch (error) {
     res.status(500).send({ message: error.message || "Error occurred" });
   }
