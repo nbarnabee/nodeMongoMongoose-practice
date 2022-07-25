9cda4c18ea1ccc3bd8bf0e78eae2fe31957ce1f4
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-07-25 14:25:05 +0200

    Add additional comments

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController.js");
 
+// We're creating an instance of a router, attaching the handlers to it, and then exporting it (see the export line at the bottom)
+
 /*
  *  App Routes
  */
